0e3d97584a6ffa7247bf96c12ffb8d2bf64c8a11
Rahul-404 <rahulshelke981@gmail.com>
2025-06-23 18:12:04 +0530

    Add demo script to execute the training pipeline

---
+++ demo.py
@@ -0,0 +1,5 @@
+from heart_stroke.pipeline.traing_pipipile import TrainingPipeline
+
+train_pipeline = TrainingPipeline()
+
+train_pipeline.run_pipeline()
--- heart_stroke/pipeline/traing_pipipile.py
+++ heart_stroke/pipeline/traing_pipipile.py
@@ -185,10 +185,3 @@ class TrainPipeline:
 
         except Exception as e:
             raise HeartStrokeException(e, sys)
-
-if __name__ == "__main__":
-    try:
-        train_pipeline = TrainPipeline()
-        train_pipeline.run_pipeline()
-    except Exception as e:
-        raise HeartStrokeException(e, sys)
